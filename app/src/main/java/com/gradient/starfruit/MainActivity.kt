330d66dda6f01dcbc66a34ecdebff9e6d16f7e0b
gradientdev <xinyunzhouu@gmail.com>
2020-10-19 23:17:38 -0400

    Updated layouts, added 3 fragments. Started working on fragment switching, not yet finished

--- app/src/main/java/com/gradient/starfruit/MainActivity.kt
+++ app/src/main/java/com/gradient/starfruit/MainActivity.kt
@@ -4,11 +4,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val aboutFragment = AboutFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        val navbar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        navbar.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+                R.id.ic_about -> makeCurrentFragment(aboutFragment)
+            }
+            true
+        }
 
         findViewById<Button>(R.id.testbutton).setOnClickListener {
             val toast = Toast.makeText(this, "Text Sent!", Toast.LENGTH_SHORT)
@@ -27,4 +46,10 @@ class MainActivity : AppCompatActivity() {
 //
 //        print(message.sid)
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, fragment)
+                commit()
+            }
 }
