777d72d3096fdb62588d96edf24b0f674950d898
gradientdev <xinyunzhouu@gmail.com>
2020-10-24 19:52:45 -0400

    Starfruit is now able to send SMS messages!

--- app/src/main/java/com/gradient/starfruit/MainActivity.kt
+++ app/src/main/java/com/gradient/starfruit/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.gradient.starfruit
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.SmsManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -28,16 +32,28 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        //todo: add text function here
+        //todo: get sms permissions
 
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.SEND_SMS) !==
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                            Manifest.permission.SEND_SMS)) {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(Manifest.permission.SEND_SMS), 1)
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(Manifest.permission.SEND_SMS), 1)
+            }
+        }
 
+        //todo: sends the message
 
         val smsManager: SmsManager = SmsManager.getDefault()
-
-        smsManager.sendTextMessage("+12672747668", null, "hello!", null, null);
-
+        smsManager.sendTextMessage("+12672747668", null, "Hello from Starfruit! \uD83C\uDF20", null, null);
 }
-        private fun makeCurrentFragment(fragment: Fragment) =
+
+    private fun makeCurrentFragment(fragment: Fragment) =
                 supportFragmentManager.beginTransaction().apply {
                     replace(R.id.fl_wrapper, fragment)
                     commit()
