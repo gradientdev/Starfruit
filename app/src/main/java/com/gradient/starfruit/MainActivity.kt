da6a5a2b84527018b8338d500156e32124b761bd
gradientdev <xinyunzhouu@gmail.com>
2020-10-28 15:01:53 -0400

    Starting to implement AlarmManager

--- app/src/main/java/com/gradient/starfruit/MainActivity.kt
+++ app/src/main/java/com/gradient/starfruit/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.gradient.starfruit
 
 import android.Manifest
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.telephony.SmsManager
 import androidx.appcompat.app.AppCompatActivity
@@ -52,6 +59,37 @@ class MainActivity : AppCompatActivity() {
 //        smsbutton.setOnClickListener{ sendMessage("If you got this, SMS seems to be working!") }
 
         //todo: start AlarmManger to trigger message
+        //todo: https://developer.android.com/training/scheduling/alarms
+
+        val receiver = ComponentName(this, SampleBootReceiver::class.java)
+
+        this.packageManager.setComponentEnabledSetting(
+                receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP
+        )
+
+        var alarmMgr: AlarmManager? = null
+        lateinit var alarmIntent: PendingIntent
+        alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
+            PendingIntent.getBroadcast(this, 0, intent, 0)
+        }
+
+        // Setting the alarm to start at 7:15 a.m. This is a placeholder/default, time will be custom in the future
+        val calendar: Calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, 7)
+            set(Calendar.MINUTE, 15)
+        }
+
+        // setRepeating gives custom time interval, currently set to 24 hours using INTERVAL_DAY
+        alarmMgr?.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY,
+                alarmIntent
+        )
 
         //todo: set up NumberPicker
 
@@ -84,4 +122,14 @@ class MainActivity : AppCompatActivity() {
                     replace(R.id.fl_wrapper, fragment)
                     commit()
                 }
+
+    //todo: retrigger alarm if device restarted
+    class SampleBootReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+                // Set the alarm here.
+            }
+        }
+    }
 }
