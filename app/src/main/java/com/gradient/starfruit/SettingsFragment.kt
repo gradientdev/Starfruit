284b989b292a57806f7b1f0e117da2ac04ef96f0
gradientdev <xinyunzhouu@gmail.com>
2020-11-07 19:21:23 -0500

    Large update, app now saves phone number persistently!

--- app/src/main/java/com/gradient/starfruit/SettingsFragment.kt
+++ app/src/main/java/com/gradient/starfruit/SettingsFragment.kt
@@ -1,14 +1,16 @@
 package com.gradient.starfruit
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.net.ParseException
 import android.os.Bundle
 import android.os.Vibrator
+import android.preference.PreferenceManager
 import android.telephony.SmsManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_settings.view.*
@@ -23,6 +25,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@Suppress("DEPRECATION")
 class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -43,14 +46,45 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        var below1 = view.phonePicker as EditText
-        var phoneNum = below1.text
+        val phonePicker = view.phonePicker
+
+        try {
+            val preferences: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(context)
+            val num: String = (preferences.getString("phoneNumber", "").toString())
+
+            if (num != null) {
+                view.phonePicker.setText(num)
+            }
+        } catch (e: ParseException) {
+        }
+
+        view.saveButton.setOnClickListener { view ->
+            var phoneNum = phonePicker.text.toString()
+            Log.d("smsSave", "Selected")
+            Log.d("smsSave", phoneNum)
+
+            val preferences: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(context)
+            preferences.edit().putString("phoneNumber", phoneNum).apply();
+
+            Toast.makeText(context, "Number saved!", Toast.LENGTH_LONG).show()
+            val vibrator =
+                context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator; vibrator.vibrate(
+            150)
+        }
 
         view.smsButton.setOnClickListener { view ->
-            Log.d("btnSetup", "Selected")
+            Log.d("smsSend", "Selected")
             val smsManager = SmsManager.getDefault()
+
+            //gets phone number saved earlier from preferences
+            val preferences: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(context)
+            val num = (preferences.getString("phoneNumber", "").toString())
+
             smsManager.sendTextMessage(
-                "${phoneNum}",
+                "${num}",
                 null,
                 "This is an SMS test message from Starfruit! \uD83C\uDF86",
                 null,
@@ -59,8 +93,7 @@ class SettingsFragment : Fragment() {
             Toast.makeText(context, "Message sent!", Toast.LENGTH_LONG).show()
             val vibrator =
                 context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator; vibrator.vibrate(
-            150
-        )
+            150)
         }
 
         // Return the fragment view/layout
